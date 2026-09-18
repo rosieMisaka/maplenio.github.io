20541ffe1293503ff87e90ae03ca84d1800c6ac4
Rosie <roseliu123abc@gmail.com>
2026-09-18 18:56:19 -0400

    Update index page
    1. nav category有背景遮盖下方object，并改了一下padding layout
    2. toTop和side filter下滑才会显示，上滑隐藏
    3. js增加method去检测用户输入
    4. 修改nav cat, title和side filter的遮盖关系z-index
    5. 改了一下<a>相关的text style在css file最顶端

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Navigation Category check active category
 let activeCat = 'all';
 
 document.getElementById('pageCat').addEventListener('click', e=>{
@@ -13,6 +14,7 @@ document.getElementById('pageCat').addEventListener('click', e=>{
     console.log("active Cat is:",activeCat);
 });
 
+// temperory code for template, will remove and retrive picture from backend
 const listings = [
     { title:"Used EarPod Pro 2019", price:25, cat:"electric",
         img:"https://images.unsplash.com/photo-1600294037681-c80b4cb5b434?w=600&q=80&auto=format&fit=crop" },
@@ -23,9 +25,9 @@ const listings = [
     { title:"IKEA second hand electric desk", price:25, cat:"furniture",
         img:"https://images.unsplash.com/photo-1518455027359-f3f8164ba6bd?w=600&q=80&auto=format&fit=crop" },
     { title:"Winter down jacket, size M", price:30, cat:"clothes",
-        img:"https://images.unsplash.com/photo-1544966503-7cc5ac882d5f?w=600&q=80&auto=format&fit=crop" },
+        img:"https://images.unsplash.com/photo-1591047139829-d91aecb6caea?q=80&w=1336&auto=format&fit=crop" },
     { title:"Rice cooker, barely used", price:18, cat:"electric",
-        img:"https://images.unsplash.com/photo-1585237017125-24baf8d7406f?w=600&q=80&auto=format&fit=crop" },
+        img:"https://images.unsplash.com/photo-1544233726-9f1d2b27be8b?q=80&w=987&auto=format&fit=crop" },
     { title:"Study desk lamp", price:10, cat:"daily",
         img:"https://images.unsplash.com/photo-1507473885765-e6ed057f782c?w=600&q=80&auto=format&fit=crop" },
     { title:"Ceramic dinnerware set", price:20, cat:"daily",
@@ -50,6 +52,7 @@ const listings = [
         img:"https://images.unsplash.com/photo-1603199506016-b9a594b593c0?w=600&q=80&auto=format&fit=crop" }
 ];
 
+// 生成 grid list
 const grid = document.getElementById('grid');
 grid.innerHTML = '';
 listings.forEach(item=>{
@@ -64,4 +67,29 @@ listings.forEach(item=>{
         <p class="card-price">$${item.price}</p>
       `;
     grid.appendChild(card);
+});
+
+// Search bar search input detection
+document.getElementById('searchInput').addEventListener('keydown', e=>{
+
+    console.log("User input is: ");
+    if(e.key === 'Enter') {
+        query = e.target.value.trim();
+        console.log(query);
+    }
+});
+
+
+// scroll to top button
+const toTop = document.getElementById('toTop');
+window.addEventListener('scroll', ()=>{
+    toTop.classList.toggle('show', window.scrollY > 200);
+});
+toTop.addEventListener('click', ()=> window.scrollTo({top:0, behavior:'smooth'}));
+
+
+// scroll to show side filter
+const sideFilter = document.getElementById('sideFilter');
+window.addEventListener('scroll', ()=>{
+    sideFilter.classList.toggle('apps', window.scrollY > 200);
 });
